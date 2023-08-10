cceb5f0c4f3c67526581e07af6f47575a5e578b1
yuomc98@gmail.com <yuomc98@gmail.com>
2023-08-10 10:40:28 +0900

    commit

--- mysite/board/views.py
+++ mysite/board/views.py
@@ -2,11 +2,14 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from .models import DataEngCsv
 from .serializers import TestDataSerializer
-
+from django.shortcuts import render
 
 @api_view(['GET'])
 def getTestDatas(request):
     datas = DataEngCsv.objects.all()
     serializer = TestDataSerializer(datas, many=True)
     return Response(serializer.data)
+
+# 
+
 
--- mysite/single_page/views.py
+++ mysite/single_page/views.py
@@ -12,3 +12,13 @@ def about_me(request):
         request,
         'single_page/about_me.html'
     )
+
+# from ..board.views import DataEngCsv
+# # board 폴더에서 views파일에 있는 DataEngCsv 클래스 사용
+
+# def test_2(request):
+#     zip_code = DataEngCsv.objects.first()
+#     context = {'zipcode':zip_code}
+
+#     return render(request,'single-page/landing.html',context)
+
