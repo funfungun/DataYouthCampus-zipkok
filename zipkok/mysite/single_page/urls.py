54a3b4f9487762ecc9c728f6cb4ddb89d3bb085e
funfun <alpha5179@naver.com>
2023-09-04 20:03:10 +0900

    last commit

---
+++ zipkok/mysite/single_page/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from . import views
+from django.conf.urls.static import static
+from django.conf import settings
+
+urlpatterns = [
+    path('', views.index),
+    path('check_and_filter/',views.first, name = 'first'),
+    path('category/',views.check_and_filter, name='check_and_filter'),
+    path('input/',views.category,name='category'),
+    path('result/',views.final_page,name='final_page'),
+    path('result/real_estate/',views.real_estate,name='real_estate'),
+]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
